9b9dcaecbd49afdac17431023091bf326d20c7b1
realgrm <gabriel.reis.mattoso@gmail.com>
2020-11-10 02:07:16 -0300

    Update podman-desktop-file-copy-to-user.sh

--- podman-desktop-file-copy-to-user.sh
+++ podman-desktop-file-copy-to-user.sh
@@ -4,11 +4,12 @@
 # OBS: Isto é um esboço, não seguindo a linguagem
 
 # variáveis iniciais
-user=realgrm
-container_id = d0ed434fec06789afb233cd607dee2572efe8f30bf816c7edddc9372b4bd668f
-desktop_source_path = "home/" && user && "local/share/containers/storage/overlay/" && container_id &&"/diff/usr/share/applications/"
-desktop_destination_path = "home/" && user && "local/share/applications/"
-icon_link_path: "/home/" && user && "/.local/share/containers/storage/overlay/d0ed434fec06789afb233cd607dee2572efe8f30bf816c7edddc9372b4bd668f/diff/usr/share/icons/hicolor/scalable/apps/"
+container_id="d0ed434fec06789afb233cd607dee2572efe8f30bf816c7edddc9372b4bd668f"
+container_path="${HOME}/.local/share/containers/storage/overlay/${container_id}/"
+desktop_source_path="${container_path}/diff/usr/share/applications/"
+desktop_destination_path="${HOME}/.local/share/applications/"
+icon_path="container_path/diff/usr/share/icons/hicolor/scalable/apps/"
+
 
 # ------Detectar novo programa instalado------
 # Verificar quando houver um novo arquivo em dektop_source_path 
@@ -16,7 +17,7 @@ icon_link_path: "/home/" && user && "/.local/share/containers/storage/overlay/d0
 # OBS: 5 minutos pode ser alterado, escolhido valor alto, para caso sejam criados ou colados mais de um arquivo ao mesmo tempo e caso o script demore até 5min a ser acionado
 
 # Para cada desktop_source_name_ext em desktop_source_name_ext_list 
-desktop_destination = desktop_destination_path && "toolbox-" && desktop_source_name_ext
+
 
 # verificar se termina com .desktop
 	# TRUE: verificar se já existe sua cópia em desktop_destination
@@ -27,30 +28,34 @@ desktop_destination = desktop_destination_path && "toolbox-" && desktop_source_n
 # ------Alternativa: escolher arquivo manualmente------
 
 # Abrir pasta origem
-xdg-open desktop_source_path
+ls -o -tr ${desktop_source_path} | grep ".*\.desktop$" | awk '{print $5.$6,$7,$8}' | tail -15
+echo "Últimos 15 alterados, copie e cole aqui o nome com extensão:"
+#read desktop_filename
+desktop_filename="firefox.desktop"
+#xdg-open "${desktop_source_path}${desktop_filename}"
 
-# Utilizar zenith para pedir o usuário o nome do arquivo (sem extensão), com resultado a variavel desktop_source_name
-
-# Com isto definir o arquivo de origem e destino (com o prefixo toolbox-)
-desktop_source = desktop_source_path && desktop_source_name && ".desktop"
-desktop_destination = desktop_destination_path && "toolbox-" && desktop_source_name && ".desktop"
-
-# Executar função copy_desktop_file
 
 # ------Função copy_desktop_file------
 
 # copiar para aplicações do usuário
-cp desktop_source desktop_destination
+desktop_destination_file="${desktop_destination_path}toolbox-${desktop_filename}"
+cp "${desktop_source_path}${desktop_filename}" "${desktop_destination_file}"
 
-# Dentro do arquivo, mudar linha de execução
-# Procurar por "\nExec=" e substituir por "Exec= toolbox run"
+echo "${desktop_source_path}"
 
-# Dentro do arquivo, mudar linha de ícone
-# Extrair nome do ícone e atribuir a icon_name
-# Definir destino do ícone
-icon_link = icon_link_path && icon_name && ".svg"
-# Substituir linha por "Icon=" && icon_link
+# Comando de execução
 
-# Salvar arquivo destino?
+before="Exec="
+after="Exec=toolbox run "
+
+sed -i "s|${before}|${after}|g" ${desktop_destination_file}
+
+# Caminho para o ícone
+
+before="Icon="
+after="Icon=${icon_path}"
+
+sed -i "s|${before}|${after}|g" ${desktop_destination_file}
+
 
 # ------Fim da função------
