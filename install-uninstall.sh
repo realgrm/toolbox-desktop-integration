9299325ba8046153244d29a2538e62619f692238
realgrm <gabriel.reis.mattoso@gmail.com>
2020-11-09 22:56:25 -0300

    Create install-uninstall.sh

---
+++ install-uninstall.sh
@@ -0,0 +1,17 @@
+# -----Install-----
+
+# Create folder and copy scripts
+mkdir ~/.podman-desktop-file-copy-to-user
+cp file1 ~/.podman-desktop-file-copy-to-user
+cp file2 ~/.podman-desktop-file-copy-to-user
+
+# Make aliases to run scripts
+alias script-install-app "sudo dnf install" 
+alias script-remove-app 
+alias script-remove-script 
+
+# -----Uninstall-----
+
+# Remove folder created folder
+
+# Remove created aliases
