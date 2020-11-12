42f299989b3278ffa808f4857c506e8013f8726a
realgrm <gabriel.reis.mattoso@gmail.com>
2020-11-12 02:55:21 -0300

    Update install-uninstall.sh

--- install-uninstall.sh
+++ install-uninstall.sh
@@ -15,3 +15,25 @@ alias script-remove-script
 # Remove folder created folder
 
 # Remove created aliases
+
+
+
+# manual input of folder
+overlay-id = "d0ed434fec06789afb233cd607dee2572efe8f30bf816c7edddc9372b4bd668f"
+
+# get toolbox id
+toolbox-id = toolbox list | grep running | awk '{print $1}'
+
+# create backup
+mv "${HOME}/.local/share/containers/storage/overlay/${overlay-id}/diff/usr/share/applications" \
+	"${HOME}/.local/share/containers/storage/overlay/${overlay-id}/diff/usr/share/applications-backup"
+
+# create folder inside applications (host) to current toolbox
+mkdir "${HOME}/.local/share/applications/toolbox-${toolbox-id}"
+
+# create symbolic link
+ln -s "${HOME}/.local/share/applications/toolbox-${toolbox-id}" \
+	"${HOME}/.local/share/containers/storage/overlay/${overlay-id}/diff/usr/share/applications-backup"
+
+# copy contents of the backup
+cp 
