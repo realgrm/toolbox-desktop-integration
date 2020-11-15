4202649d20523981c9022a55dc2dded8215e2a6c
realgrm <gabriel.reis.mattoso@gmail.com>
2020-11-14 22:27:18 -0300

    Update install.py

--- install.py
+++ install.py
@@ -2,26 +2,77 @@
 
 from pathlib import Path
 import os
+import subprocess
+import sys
 
 # _______________________________________________________________
 
 # declare variables
-link_app = os.environ['HOME']+"/.local/share/applications/toolbox"
-link_icon = os.environ['HOME']+"/.local/share/icons/toolbox"
-l=os.environ['HOME']+"/.local/share/folder/toolbox/"
-overlay_id=""
-c=os.environ['HOME']+"/.local/share/containers/storage/overlay/"
+l=os.environ['HOME']+"/.local/share/folder/toolbox"
+c_usrshare=""
+c_overlay=os.environ['HOME']+"/.local/share/containers/storage/overlay/"
+file_check="/diff/usr/share/"
+#/var/home/realgrm/.local/share/containers/storage/overlay/b4fa6ff1346dec95ce4454464201fdadfd816e10eb7322048829c551ce032d08/diff
+
+# _______________________________________________________________
+
+# defining functions
+
+# to show informations to the user
+
+def msg_link_created():
+    print("created symlink:\n"+click+"\n\nthat redirects to:\n"+havetolink)
+
+def msg_link_ok():
+    print("all ok, symlink already exists:\n"+click+"\n\nit redirects to:\n"+linksto)
+
+def msg_link_nok():
+    print("can't create symlink, because another symlink already exists:\n"+click+"\n\nit redirects to:\n"+linksto)
+
+def msg_file_nok():
+    print("can't create symlink, because file/folder already exists\n"+click+"\n\nplease delete (with backup?) or rename it, then try again.")
+
+def msg_container_nok():
+    print("Container not found.\nCreate one with the command \"toolbox create\" in the terminal")
+
+def msg_container_file_nok():
+    print("File that check  not found.\nMake sure a container is created using the command \"toolbox list\" or try to change the file_check variable")
+
+# to show debug info
+
+def msg_debug():
+    print("debug locations:")
+    print("where do i click (exists: "+str(Path(click).exists())+" is symlink: "+str(Path(click).is_symlink())+")\n"+click)
+    print("have to link:\n"+havetolink)
+    print("currently links to:\n"+linksto)
+    print("click=link"+str(click==linksto))
+    print("link ok: "+str(havetolink==linksto))
+
 
 # _______________________________________________________________
 
 # get the correct folder to the container
 
-os.system("/usr/bin/toolbox run")
+if not Path(c_overlay).is_dir():
+    sys.exit(msg_container_nok())
 
-for d in os.listdir(c):
-    if Path(c+d+"/diff/run/.containerenv").is_file():
-        overlay_id=c+d+"/diff/usr/share/"
-    else: pass
+for id in os.listdir(c_overlay):
+    i=c_overlay+id+file_check
+    j=i+"applications"
+    print(str(Path(j).exists())+": "+j)
+    
+    if Path(j).is_dir():
+        c_usrshare=i
+
+if c_usrshare == "": 
+    sys.exit(msg_container_file_nok())
+
+# _______________________________________________________________
+
+print("\n\n_____________________")
+print("\nINSTALL DESKTOP FILES SCRIPT")
+print("_____________________")
+print("\nfolder of container: \n"+c_usrshare)
 
 # _______________________________________________________________
 
@@ -30,16 +81,34 @@ for d in os.listdir(c):
 a=["applications","icons"]
 
 for i in a:
-    print("\n\n"+i+"\n")
-    scr=l.replace("folder",i)   
-    dst=overlay_id+i+"/"
-    if str(Path(scr).resolve())==scr:
-        print("created symlink:\n"+scr+"\n"+dst)
-        os.symlink(scr, dst)
-    else: print("symlink already exists:\n"+scr+"\n"+str(Path(scr).resolve()))
+    print("\n"+"_____________________\n")
+    print(i+"\n")
+    click=l.replace("folder",i)   
+    havetolink=c_usrshare+i
+    linksto=str(Path(click).resolve())
+
+    #uncomment the line bellow to show more info
+    #msg_debug
+
+    if not Path(click).exists() and click==linksto:
+        os.symlink(havetolink, click)
+        msg_link_created()
+    elif Path(click).is_symlink():
+        if havetolink==linksto:
+            msg_link_ok()
+        else: msg_link_nok()
+    else: msg_file_nok()
+
+# _______________________________________________________________
+
+print("\n")
+
+import update_desktop_files
+
+
+
+
 
 #Applications
 # create link in usr/share
-#os.symlink(src, dst)
-#os.symlink(src, dst)
 # create backup
