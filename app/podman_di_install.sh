a0ed74580af60279b4473e2a1abbafb43e6a6b5e
Gabriel Mattoso <gabriel.reis.mattoso@gmail.com>
2021-05-21 12:08:26 -0300

    Update podman_di_install.sh

--- app/podman_di_install.sh
+++ app/podman_di_install.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 DESKTOPFILE="podman_di.desktop"
+DESKTOPFOLDER="$HOME/.local/share/applications/"
+ICONFILE="podman_di.png"
+ICONFOLDER="$HOME/.local/share/icons/hicolor/1024x1024/apps"
 UPDATEFILE="podman_di_update.sh"
 UPDATEFOLDER="$HOME/.local/bin"
+THISFOLDER=$(dirname $(realpath $0))
 
-cp -f $(dirname $(realpath $0))/$UPDATEFILE \
-	$UPDATEFOLDER/
+cp -f $THISFOLDER/$UPDATEFILE $UPDATEFOLDER/
 
 [[ $PATH != *$UPDATEFOLDER* ]] && \
 	PATH=$UPDATEFOLDER:$PATH
 
 chmod +x $UPDATEFOLDER/$UPDATEFILE
 
-cp -f $(dirname $(realpath $0))/$DESKTOPFILE \
-	$HOME/.local/share/applications/
+cp -f $THISFOLDER/$DESKTOPFILE $DESKTOPFOLDER
 
-sed -i "s/^Exec=/Exec=$UPDATEFILE/" \
-	$HOME/.local/share/applications/
+sed -i "s/^Exec=/Exec=$UPDATEFILE/" $DESKTOPFOLDER
+
+mkdir -p $ICONFOLDER
+
+cp -f $THISFOLDER/$ICONFILE $ICONFOLDER
+
 
 $UPDATEFILE
